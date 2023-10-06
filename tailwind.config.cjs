0fc102443574ad62b09ea246c428015fb5c32050
vicenzogiordana <giordanavicenzo@gmail.com>
2023-10-06 16:08:22 -0300

    landing page y agregado del embeed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  mode: 'jit',
   theme: {
     extend: {
       animation: {
         'spin-slow': 'spin 5s linear infinite',
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
       },
     },
   },
-  plugins: [],
-}
+  variants: {},
+  plugins: [require('tailwind-scrollbar-hide')],
+};
+
