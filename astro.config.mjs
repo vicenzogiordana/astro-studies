30bc3d94a40dec93bf68f5b524c50f449b4f5d10
vicenzogiordana <giordanavicenzo@gmail.com>
2023-11-08 21:53:44 -0300

    netli

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import cloudflare from '@astrojs/cloudflare';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server',
-  adapter: vercel()
+  adapter: netlify()
 });
