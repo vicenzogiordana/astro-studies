7c4183107992f9548c92afa1458fa4254686303f
vicenzogiordana <giordanavicenzo@gmail.com>
2023-11-08 21:25:21 -0300

    primerDeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
+import cloudflare from '@astrojs/cloudflare';
 
-import cloudflare from '@astrojs/cloudflare'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server',
-  adapter: cloudflare(),
-})
+  adapter: vercel()
+});
--- src/pages/api/courses.json.ts
+++ src/pages/api/courses.json.ts
@@ -1,48 +1,54 @@
 const courses = [
   {
     id: 0,
+    name: "¿Dios existe?",
+    artist: "Joel Flores",
+    imgUrl: "/covers/existencia.jpg"
+  },
+  {
+    id: 1,
     name: "¿Por qué creer en la Biblia?",
     artist: "Joel Flores",
     imgUrl: "/covers/biblia4.jpg"
   },
   {
-    id: 1,
+    id: 2,
     name: "¿Dónde está Dios cuando sufrimos?",
     artist: "Joel Flores",
     imgUrl: "/covers/sufrimiento4.jpg"
   },
   {
-    id: 2,
+    id: 3,
     name: "Oración y fe",
     artist: "Joel Flores",
     imgUrl: "/covers/oracion5.jpg"
   },
   {
-    id: 3,
+    id: 4,
     name: "Perdón de los pecados",
     artist: "Joel Flores",
     imgUrl: "/covers/cruz.jpg"
   },
   {
-    id: 4,
+    id: 5,
     name: "La segunda venida",
     artist: "Joel Flores",
     imgUrl: "/covers/segundaVenida2.jpg"
   },
   {
-    id: 5,
+    id: 6,
     name: "La ley de Dios",
     artist: "Joel Flores",
     imgUrl: "/covers/mandamientos2.jpg"
   },
   {
-    id: 6,
+    id: 7,
     name: "El día de descanso",
     artist: "Joel Flores",
     imgUrl: "/covers/sabado3.jpg"
   },
   {
-    id: 7,
+    id: 8,
     name: "El bautismo",
     artist: "Joel Flores",
     imgUrl: "/covers/bautismo2.jpg"
