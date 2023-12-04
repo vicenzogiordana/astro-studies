d62280ed0bd9e65beb95b9bd753e9471209cc6ff
vicenzogiordana <giordanavicenzo@gmail.com>
2023-12-04 16:41:08 -0300

    </ (generated by blackbox.ai )

---
+++ src/components/icons.tsx
@@ -0,0 +1,188 @@
+export function IconFacebook(props) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+      </svg>
+    )
+  }
+  export function IconWhatsApp(props) {
+    return (
+      <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+       width="24" height="24" viewBox="0 0 512.000000 512.000000"
+       preserveAspectRatio="xMidYMid meet">
+      
+      <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+      fill="currentColor" stroke="currentColor">
+      <path d="M2370 5114 c-19 -2 -78 -9 -130 -15 -788 -90 -1517 -582 -1919 -1296
+      -351 -624 -416 -1405 -175 -2095 20 -57 67 -168 105 -247 l69 -144 -160 -573
+      c-88 -316 -160 -585 -160 -599 0 -69 76 -145 145 -145 23 0 185 44 941 256
+      l230 64 145 -69 c647 -310 1379 -333 2059 -64 626 247 1172 795 1418 1424 340
+      872 195 1834 -385 2554 -409 507 -974 829 -1633 930 -100 15 -472 28 -550 19z
+      m395 -304 c583 -54 1146 -347 1517 -790 225 -268 393 -596 471 -921 49 -201
+      61 -309 61 -539 0 -230 -12 -338 -61 -539 -78 -325 -246 -653 -471 -921 -296
+      -354 -732 -624 -1183 -733 -201 -49 -309 -61 -539 -61 -388 0 -658 63 -1017
+      240 -100 49 -184 84 -202 84 -17 0 -240 -59 -497 -130 -257 -72 -469 -129
+      -471 -126 -2 2 55 213 127 469 71 257 130 480 130 498 0 19 -32 95 -83 197
+      -155 311 -229 593 -243 932 -45 1013 604 1936 1575 2244 282 89 592 123 886
+      96z"/>
+      <path d="M1800 3939 c-30 -5 -89 -25 -130 -45 -64 -30 -92 -53 -195 -157 -133
+      -135 -187 -218 -236 -360 -38 -110 -49 -188 -49 -338 0 -240 54 -448 182 -701
+      213 -422 584 -785 1009 -988 223 -106 398 -150 624 -157 187 -6 301 12 432 69
+      117 51 187 101 309 222 91 91 118 125 147 186 70 144 70 286 0 430 -30 63 -57
+      96 -162 201 -101 101 -139 132 -191 156 -205 96 -402 61 -579 -103 -83 -77
+      -115 -93 -153 -74 -32 17 -506 489 -524 522 -24 46 -17 65 60 149 41 45 89
+      108 105 139 79 147 80 305 3 460 -35 70 -229 272 -307 319 -107 65 -231 90
+      -345 70z m169 -313 c14 -8 72 -61 128 -119 92 -94 102 -108 108 -152 12 -75
+      -7 -117 -92 -208 -116 -123 -159 -241 -135 -369 22 -118 42 -144 355 -455 314
+      -312 331 -325 451 -345 128 -21 243 21 358 131 80 76 126 101 183 101 60 0 96
+      -23 201 -131 81 -82 104 -113 114 -147 23 -86 2 -128 -127 -253 -108 -105
+      -178 -146 -294 -174 -83 -19 -264 -19 -368 0 -403 77 -830 379 -1106 785 -68
+      100 -164 293 -199 403 -79 245 -75 509 11 670 39 73 217 254 268 272 44 16
+      101 13 144 -9z"/>
+      </g>
+      </svg>
+      
+    )
+  }
+  
+  
+  export function IconInstagram(props) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+        <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+      </svg>
+    )
+  }
+  
+  
+  export function IconYouTube(props) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" />
+        <path d="m10 15 5-3-5-3z" />
+      </svg>
+    )
+  }
+  export function IconAbout(props) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <path d="M12 16v-4" />
+        <path d="M12 8h.01" />
+        {/* Add a small circle for the dot on the "i" */}
+        <circle cx="12" cy="9" r="1" />
+      </svg>
+    );
+  }
+  
+  
+  
+  export function IconHome(props) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+        <polyline points="9 22 9 12 15 12 15 22" />
+      </svg>
+    )
+  }
+  
+  
+  export function IconNews(props) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
+        <path d="M18 14h-8" />
+        <path d="M15 18h-5" />
+        <path d="M10 6h8v4h-8V6Z" />
+      </svg>
+    )
+  }
+  
+  
+  export function IconVideo(props) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="m22 8-6 4 6 4V8Z" />
+        <rect width="14" height="12" x="2" y="6" rx="2" ry="2" />
+      </svg>
+    )
+  }
